a09ca2589c569a0ebf2c70b7a71c09f4cc372b8e
stdg3 <velikoglosv@mail.ru>
2023-12-13 11:29:53 +0200

    intro

---
+++ someTricks/01intro.py
@@ -0,0 +1,15 @@
+# setting to auto pep
+my_list = [1, 'True', True]
+
+# shift alt f => document format
+print(my_list)
+
+# settings => format on save = enable
+# default formatter = python
+
+
+def my_func(eben: str):
+    print('sikimsonik', eben)
+
+
+my_func('ebenbebenebeben')
