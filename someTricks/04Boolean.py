a99298132a8282387ad96d258a614e6fa25ba135
stdg3 <velikoglosv@mail.ru>
2023-12-14 17:57:48 +0200

    bollean type

---
+++ someTricks/04Boolean.py
@@ -0,0 +1,22 @@
+print(2 > 3)
+# False
+
+print('Py' > 'Python')
+# sembolik karsilastirma
+# False
+
+print(bool('py'))
+# True
+
+print('-' * 5)
+
+print(bool(2))
+# True
+print(bool('py'))
+# True
+print(bool([]))
+# False
+print(bool([0, 2]))
+# True
+print(bool(None))
+# False
