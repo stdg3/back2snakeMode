d1a22be1d982574b121976f613c78599c7688c6e
stdg3 <velikoglosv@mail.ru>
2023-12-14 17:48:21 +0200

    numeric conversation

---
+++ someTricks/03NumConvertation.py
@@ -0,0 +1,15 @@
+num = int(input('num: '))
+
+print(type(num))
+
+
+my_float = 3.14
+
+print(type(my_float))
+# float
+
+str_temperature = '14.50'
+
+temperature = float(str_temperature)
+
+print(f"temperatur: {temperature} {type(temperature)}")
